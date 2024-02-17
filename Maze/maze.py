ff252253c8e474eb49c9925c5f14a113b9defb0c
Mahak Agrawal <132470319+Mahak-Agrawal-304@users.noreply.github.com>
2024-02-17 15:51:22 +0530

    added function to solve the maze

--- Maze/maze.py
+++ Maze/maze.py
@@ -74,4 +74,83 @@ class Maze():
         
         self.solution=None
     
-    def 
+        def print(self):
+        solution=self.solution[1] if self.solution is not None else None
+        print()
+        for i, row in enumerate(self.walls):
+            for  j, col in enumerate(row):
+                if col:
+                    print("#", end="")
+                elif (i,j)==self.start:
+                    print("A", end="")
+                elif (i,j)==self.goal:
+                    print("B", end="")
+                elif solution is not None and (i,j) in solution:
+                    print(".", end="")
+                else:
+                    print(" ",end="")
+            print()
+        print()
+        
+    def neighbors(self, state):
+        row, col=state
+
+        candidates=[
+            ("up", (row-1, col)),
+            ("down", (row+1, col)),
+            ("left", (row, col-1)),
+            ("right", (row, col+1))
+        ]
+
+        result=[]
+        for action , (r,c) in candidates:
+            try:
+                if not self.walls[r][c]:
+                    result.append((action, (r,c)))
+            except IndexError:
+                continue
+        return result
+
+    def solve(self):
+        self.num_explored=0
+        start=Node(state=self.start, parent=None, action=None)
+        frontier=StackFrontier()        # using depth-first search algorithm
+        frontier.add(start)
+
+        self.explored=set()
+
+        while True:
+            if frontier.empty():
+                raise Exception("No solution")
+            node=frontier.remove()
+            self.num_explored += 1
+
+            if node.state==self.goal:
+                actions=[]
+                cells=[]
+                while node.parent is not None:
+                    actions.append(node.action)
+                    cells.append(node.state)
+                    node=node.parent
+                actions.reverse()
+                cells.reverse()
+                self.solution=(actions,cells)
+                return
+            self.explored.add(node.state)
+
+            for action,state in self.neighbors(node.state):
+                if not frontier.contains_state(state) and state not in self.explored:
+                    child=Node(state=state, parent=node, action=action)
+                    frontier.add(child)
+
+if len(sys.argv) != 2:
+    sys.exit("Usage: python maze.py maze.txt")
+
+m=Maze(sys.argv[1])
+print("Maze: ")
+m.print()
+print("Solving...")
+m.solve()
+print("States explored: ", m.num_explored)
+print("Solution: ")
+m.print()
